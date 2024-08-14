8dd203550448cfd082ea92a8d6a5e4585fbceae3
ibnuhaji66 <ibnuhaji66@gmail.com>
2024-08-14 09:50:48 +0700

    Deklarasi

---
+++ deklarasi.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int x;                // Deklarasi variabel global 
+int main()
+{
+    x=10;                 // inisialisasi pertama
+    cout <<"first value of x = "<< x;
+    x=20;                 // inisialisasi kedua
+    cout <<"diinisialisasi lagi dengan value = "<< x;
+}
