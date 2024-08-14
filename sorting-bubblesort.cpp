65f9a184f185f1900c7bc30f46a8961c9f867255
ibnuhaji66 <ibnuhaji66@gmail.com>
2024-08-14 09:44:29 +0700

    Sorting Bubble Sort

---
+++ sorting-bubblesort.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+//bubble Short
+
+int arr[] = {37,29,14,10,14};
+int length = 5;
+
+void bubbleSort(){
+	for(int i = 0; i < length; i++){
+		bool swapped = false;
+		for(int j = 0; j < length-i-1; j++){
+			if(arr[j] > arr[j+1]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				swapped = true;
+			}
+		}
+		if(swapped == false) break;
+	}
+}
+
+void print(){
+	for(int i = 0; i < length; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+int main(){
+	
+    bubbleSort();
+    
+    print();
+
+	return 0;
+}
+
