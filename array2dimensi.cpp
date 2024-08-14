71a14c19ab753b09a7c7ea8844243510cd910947
ibnuhaji66 <ibnuhaji66@gmail.com>
2024-08-14 09:38:48 +0700

    Array 2 Dimensi

---
+++ array2dimensi.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+	
+	int matrix[3][3];
+	int barisPertama = 0;
+	
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			scanf("%d", &matrix[i][j]);
+			
+		}
+	}
+	
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			printf("%d", matrix[i][j]);
+			
+		}
+		printf("\n");
+	}
+	
+	for(int i = 0; i < 3; i++){
+		barisPertama += matrix[0][i];
+	}
+    printf("Hasil tambah baris pertama : %d", barisPertama);
+	return 0;
+}
+
